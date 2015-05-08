8b29e7437f418a88f2c70d0ba60bed4d42de8091
sophia <sshoemaker@bills.com>
2015-05-07 23:25:16 -0700

    updating solution page

---
+++ wp-content/themes/moduleq/page-solution.php
@@ -0,0 +1,57 @@
+<?php get_header(); ?>
+<div class="page_content page_solution">
+<div class="outerwrapper solutionpanel_1">
+	<div class="innerwrapper solutionpanel_1">
+		<img src="<?php bloginfo('template_url'); ?>/images/solution_stairs.png">
+		<div class="content">
+		<h1>Keep Deals Humming</h1>
+		<p>Rev is a mobile application for sales professionals who<br/>
+		   need to stay on top of deals 24/7. Unlike clunky CRM <br/>
+		   tools that are designed to give information to C-level<br/> 
+		   management, Rev is a personal work tool with a<br/> 
+		   streamlined interface organized to easily drive action. </p>
+		 </div>
+		
+	</div><!-- END innerwrapper homepanel_1--> 
+</div><!-- END outerwrapper homepanel_1-->
+
+
+<div class="outerwrapper solutionpanel_2">
+
+	<div class="innerwrapper solutionpanel_2">
+		<div id="phone">
+			<img src="<?php bloginfo('template_url'); ?>/images/img_phone_screen1.png" id="phone_screen1"/>
+			<img src="<?php bloginfo('template_url'); ?>/images/img_phone_background.png" id="phone_cover">
+		</div>
+		<div class="content">
+			<h2>Accelerate Your Mojo</h2>
+			<ul>
+				<li>Stay in control of your leads by organizing them  in one place</li>
+				<li>Hit your targets by tracking next steps</li>
+				<li>Keep deals moving by responding in real time</li>
+				<li>Try out new ideas in your private work space</li>
+			</ul>
+		</div>
+		<div class="clear"></div>
+	</div>
+	
+</div>
+<div class="solutionpanel_3_white">
+	<div class="content">
+		<h2>Consider Us Your Pit Crew</h2>
+		<p>Our goal is to help you navigate your obstacles as<br/> 
+	quickly and easily as possible — giving you more time to<br/>
+	be creative and strategic. With Rev, you’ll stay focused<br/> 
+	and keep deals on track no matter where you are.</p>
+	</div>
+</div>
+<div class="solutionpanel_3_gray">
+	<div class="content">
+		<h3>We're here to help you win!</h3>
+		<a href="/support" class="rollover-button">Support</a><img src="<?php bloginfo('template_url'); ?>/images/solution_flags.png"/>
+		<div class="clear"></div>
+	</div>
+</div>
+
+<div class="clear"></div>
+<?php get_footer(); ?>
