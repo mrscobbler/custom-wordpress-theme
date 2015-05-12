951e4c2d2b3e582cc50534427fe4f8bd151e2635
sophia <sshoemaker@bills.com>
2015-05-12 00:02:20 -0700

    increasing max upload size

--- wp-content/themes/moduleq/functions.php
+++ wp-content/themes/moduleq/functions.php
@@ -1,4 +1,7 @@
 <?php
+@ini_set( 'upload_max_size' , '64M' );
+@ini_set( 'post_max_size', '64M');
+@ini_set( 'max_execution_time', '300' );
 function moduleqTheme_scripts() {
 	wp_register_script( 'validation', get_template_directory_uri() . '/js/validation.js', array( 'jquery', 'jquery-ui-core' ) );
 	wp_register_script( 'waypoints', get_template_directory_uri() . '/js/jquery.waypoints.min.js' );
