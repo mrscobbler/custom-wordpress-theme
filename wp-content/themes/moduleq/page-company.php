d52d7dc4877b7a4d05a8bcc452cd6322ee4314a3
sophia <sshoemaker@bills.com>
2015-05-12 22:56:11 -0700

    company page changes

--- wp-content/themes/moduleq/page-company.php
+++ wp-content/themes/moduleq/page-company.php
@@ -3,56 +3,58 @@
  * Template Name: Company Page
  **/
  get_header(); ?>
-<div class="outerwrapper page_content page_about">
-	<div class="innerwrapper">
-		<h1><?php echo types_render_field("page-title");?></h1>
-		<?php echo types_render_field("page-content");?>
-	</div>
-</div>	
-<div class="team-members">
-	<div class="innerwrapper">
-		<?php $taxonomy_terms = get_terms( 'assigned-team' );
-		foreach($taxonomy_terms as $term):?>
-		<hr/>
-		<h2><?php echo $term->name?></h2>
-		<div class="about-container cf">
-		<?php
-		$args = array(
-			'orderby'=>'menu_order',
-			'post_type' => 'team-member',
-			'tax_query' => array(
-				array(
-					'taxonomy' => 'assigned-team',
-					'field'    => 'slug',
-					'terms'    => $term->slug,
-				),
-			),
-
-		);
-		 $query = new WP_Query( $args );
-		 
-		 while ( $query->have_posts() ) : $query->the_post(); ?>
-		 		<div class="about-card">
-					<?php echo types_render_field("team-member-image");?>
-					<div class="about-card-info">
-						<div class="name"><?php the_title();?></div>
-						<div class="title"><?php echo types_render_field("title");?></div>
-						<hr/>
-						<ul class="accomplishments">
-							<li><?php echo types_render_field("accomplishments",array('separator'=>"</li><li>"));?></li>
-						</ul>
-						<div class="linkedin"><a href="<?php echo types_render_field("linkedin",array('output'=>'raw'));?>"><i class="fa fa-2x fa-linkedin-square"></i></a></div>
-					</div>
-			 	</div>
-		
-		  
-		 <? wp_reset_postdata();
-		 	$count++;
-
-		  endwhile; ?>
-		  
+<div class="page_content page_about">
+	<div class="outerwrapper page_content page_about">
+		<div class="innerwrapper">
+			<h1><?php echo types_render_field("page-title");?></h1>
+			<?php echo types_render_field("page-content");?>
 		</div>
-		 <?php endforeach;?>
+	</div>	
+	<div class="outerwrapper team-members">
+		<div class="innerwrapper">
+			<?php $taxonomy_terms = get_terms( 'assigned-team' );
+			foreach($taxonomy_terms as $term):?>
+			<hr/>
+			<h2><?php echo $term->name?></h2>
+			<div class="about-container cf">
+			<?php
+			$args = array(
+				'orderby'=>'menu_order',
+				'post_type' => 'team-member',
+				'tax_query' => array(
+					array(
+						'taxonomy' => 'assigned-team',
+						'field'    => 'slug',
+						'terms'    => $term->slug,
+					),
+				),
+
+			);
+			 $query = new WP_Query( $args );
+			 
+			 while ( $query->have_posts() ) : $query->the_post(); ?>
+			 		<div class="about-card">
+						<?php echo types_render_field("team-member-image");?>
+						<div class="about-card-info">
+							<div class="name"><?php the_title();?></div>
+							<div class="title"><?php echo types_render_field("title");?></div>
+							<hr/>
+							<ul class="accomplishments">
+								<li><?php echo types_render_field("accomplishments",array('separator'=>"</li><li>"));?></li>
+							</ul>
+							<div class="linkedin"><a href="<?php echo types_render_field("linkedin",array('output'=>'raw'));?>"><i class="fa fa-2x fa-linkedin-square"></i></a></div>
+						</div>
+				 	</div>
+			
+			  
+			 <? wp_reset_postdata();
+			 	$count++;
+
+			  endwhile; ?>
+			  
+			</div>
+			 <?php endforeach;?>
+			</div>
 	</div>
 </div>
   <?php get_footer();?>
