60a37ed3d39365f2d622992de1285b3fc9931812
sophia <sshoemaker@bills.com>
2015-05-14 10:53:59 -0700

    responsive issues

--- wp-content/themes/moduleq/page-support.php
+++ wp-content/themes/moduleq/page-support.php
@@ -16,7 +16,7 @@ get_header(); ?>
 </div>
 <div  class="outerwrapper page_content support_people_container">
 		<div class="innerwrapper cf">
-			<h2 style="font-size: 35px; text-align: center; font-weight: 100; margin-bottom: 40px;">Anything We Can Help You With?</h2>
+			<h2 style="">Anything We Can Help You With?</h2>
 			<p>Contact Lee or Michelle at 
 				<a style="font-weight: 400; color: #000; border-bottom: 1px solid #d1d1d1;" href="mailto:support@moduleq.com">support@moduleq.</a>
 			</p>
